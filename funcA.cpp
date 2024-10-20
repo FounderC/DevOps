7f489cc71ebfd5abc68510bbe100a26234efcfa0
Founder <141733979+FounderC@users.noreply.github.com>
2024-10-21 00:42:47 +0300

    Update funcA.cpp

--- funcA.cpp
+++ funcA.cpp
@@ -1,8 +1,7 @@
 #include "funcA.h"
 #include <cmath>
 
-double FuncA::calculate(double x, int) {
-    int n = 3; 
+double FuncA::calculate(double x, int n) {
     double sum = 0;
     for (int i = 0; i < n; ++i) {
         sum += (std::pow(x, 2 * i + 1)) / (2 * i + 1);
